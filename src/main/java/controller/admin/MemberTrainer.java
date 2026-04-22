affd89962fa77368ae31b207a39efe09e4ca3e7b
honggildong <honggildong@example.com>
2026-04-22 19:05:02 +0900

    First Commit

---
+++ src/main/java/controller/admin/MemberTrainer.java
@@ -0,0 +1,46 @@
+package controller.admin;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dto.admin.Member;
+import service.admin.MemberService;
+import service.admin.MemberServiceImpl;
+
+/**
+ * Servlet implementation class MemberTrainer
+ */
+@WebServlet("/memberTrainer")
+public class MemberTrainer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberTrainer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			MemberService service = new MemberServiceImpl();
+			List<Member> list = service.getTrainerlist();
+			request.setAttribute("list", list);
+			request.getRequestDispatcher("/adminMember/memberTrainer.jsp").forward(request, response);
+		}catch(Exception e) {
+			e.printStackTrace();
+			request.setAttribute("err", "오류가 발생했습니다.");
+			request.getRequestDispatcher("error.jsp").forward(request, response);
+		}
+	}
+}
