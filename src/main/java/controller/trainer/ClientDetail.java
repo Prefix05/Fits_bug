38c61820b23dcb1289f89488829e20c54537fb7f
Jin Ho Yon <jyon@ucdavis.edu>
2026-04-29 12:22:04 +0900

    clientMealLog data is client specific

---
+++ src/main/java/controller/trainer/ClientDetail.java
@@ -0,0 +1,18 @@
+package controller.trainer;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ClientDetail", value = "/ClientDetail")
+public class ClientDetail extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
