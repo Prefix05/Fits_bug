866ed340d66a8f4ca67f6fab8754413286d04f77
honggildong <honggildong@example.com>
2026-05-08 20:00:37 +0900

    0508 2000 admin, guideList최신화

--- src/main/java/controller/admin/AdminMain.java
+++ src/main/java/controller/admin/AdminMain.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AdminMain
  */
-@WebServlet("/admin/adminMain")
+@WebServlet("/admin/main")
 public class AdminMain extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
